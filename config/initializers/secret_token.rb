99ef3053b0ab03a1f9ab42c7ca9f6fc1372352b0
Mike Andino <mikeandinog@MIKE-laptop.(none)>
2011-03-26 00:28:00 -0600

    terminado con csss solo fue generado con scaffold y ademas el css fue copiado

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'e64aa5ef0f3059c809a800658f5f8b814bef83738e32cb804ee2bce1747c368a60a7b2e1f73e8e1402b4630a5a6ba834b5e043422d43f50f119b47dfd795af1a'
